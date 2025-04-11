4aa24823e1853293732cf12e434c01224976e45c
olesia2929 <karpina.pn@ucu.edu.ua>
2025-04-11 22:25:32 +0300

    implemention of stack using queue

--- implement_stack_using_queues/implement_stack_using_queues_solution.py
+++ implement_stack_using_queues/implement_stack_using_queues_solution.py
@@ -11,22 +11,27 @@ class MyStack:
 
     def push(self, x):
         """
-        
-        Push element x onto stack."""
+        push element x onto stack.
+        """
         self.q2.append(x)
         while self.q1:
             self.q2.append(self.q1.pop(0))
         self.q1, self.q2 = self.q2, self.q1
-
     def pop(self):
+        """
+        get the top element and remove it from the stack.
+        """
         if self.empty():
             return None
         return self.q1.pop(0)
-
     def top(self):
+        """
+        get the top element without removing it from the stack."""
         if self.empty():
             return None
         return self.q1[0]
-
     def empty(self):
+        """
+        return true if the stack is empty.
+        """
         return len(self.q1) == 0
